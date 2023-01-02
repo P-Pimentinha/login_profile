926b4e76a0adc7768aeeb6c6e8257532870a50eb
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-01-02 17:35:45 +0100

    adds dashboard/register/landing/error routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import { Landing } from './pages/index';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 function App() {
   return (
     <div>
-      <h1>Login Profile</h1>
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<div>Dashboard</div>}></Route>
+          <Route path='/register' element={<div>Register</div>}></Route>
+          <Route path='/landing' element={<Landing />}></Route>
+          <Route path='*' element={<div>error</div>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
