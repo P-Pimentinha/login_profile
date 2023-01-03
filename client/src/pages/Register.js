67a0841b68350b7a6edb051e14de3d9a98bebec4
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-01-03 10:10:35 +0100

    feat: toggle register/login

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -7,7 +7,7 @@ const initialState = {
   email: '',
   password: '',
   isMember: true,
-  showAlert: true,
+  showAlert: false,
 };
 
 const Register = () => {
@@ -22,18 +22,25 @@ const Register = () => {
     e.preventDefault();
     console.log(e.target);
   };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? 'Login' : 'register'}</h3>
         {values.showAlert && <Alert />}
-        <FormRow
-          type='text'
-          name='name'
-          value={values.name}
-          handleChange={handleChange}
-        />
+
+        {!values.isMember && (
+          <FormRow
+            type='text'
+            name='name'
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
 
         <FormRow
           type='email'
@@ -52,6 +59,12 @@ const Register = () => {
         <button type='submit' className='btn btn-block'>
           Submit
         </button>
+        <p>
+          {values.isMember ? 'Not a member?' : 'Already a member?'}{' '}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
