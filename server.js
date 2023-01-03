d210b0b233edcf2d2237f4bfdbdde0e106ea30f7
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-01-03 16:47:08 +0100

    feat: adds notFoundMiddleware

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 const app = express();
 
+//middleware
+import notFoundMiddleware from './middleware/not-found.js';
+
 app.get('/', (req, res) => {
   res.send('Welcome!');
 });
 
+app.use(notFoundMiddleware);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
