5551b496e20597752c6a771dc5be96a43e5db091
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-01-03 09:30:19 +0100

    adds FormRow Component

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
-  return <div>Error!!</div>;
+  return (
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='not found' />
+        <h3>Oh! Page not found!</h3>
+        <Link to='/'>Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { useEffect, useState } from 'react';
+import { Logo, FormRow } from '../components/index';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  //global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
